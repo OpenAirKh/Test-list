783de0f0990bc201118b371bb757291c45d3d742
Valentin Brizgalov <brizgalov.work@gmail.com>
2025-10-08 16:28:42 +0300

    add new

--- list.js
+++ list.js
@@ -62,143 +62,181 @@ const questionMassJs = [
   {
     question:
       "Объясните различия в использовании fooмежду function foo() {}иvar foo = function() {}",
+    status: false,
   },
   {
     question:
       "Можете ли вы объяснить, что такое « Function.callи Function.applyчто делать»? В чём существенная разница между ними?",
+    status: true,
   },
   {
     question: "Объяснять Function.prototype.bind.",
+    status: false,
   },
   {
     question:
       "В чем разница между обнаружением признаков, выводом признаков и использованием строки UA?",
+    status: true,
   },
   {
     question: "Объясните понятие «подъем».",
+    status: false,
   },
   {
     question:
       "Что такое приведение типов? Какие распространённые ошибки возникают при использовании приведения типов в коде JavaScript?",
+    status: true,
   },
   {
     question: "Опишите всплытие событий.",
+    status: true,
   },
   {
     question: "Опишите фиксацию событий.",
+    status: false,
   },
-  {
-    question: "В чем разница между «атрибутом» и «свойством»?",
-  },
+  { question: "В чем разница между «атрибутом» и «свойством»?", status: false },
   {
     question:
       "Каковы плюсы и минусы расширения встроенных объектов JavaScript?",
+    status: false,
   },
   {
     question: "В чем разница между ==и ===?",
+    status: false,
   },
   {
     question: "Объясните политику единого источника в отношении JavaScript.",
+    status: true,
   },
   {
     question:
       "Почему оператор называется тернарным, что означает слово «тернарный»?",
+    status: false,
   },
   {
     question:
       "Что такое строгий режим? Каковы преимущества и недостатки его использования?",
+    status: true,
   },
   {
     question:
       "Каковы некоторые преимущества/недостатки написания кода JavaScript на языке, который компилируется в JavaScript?",
+    status: false,
   },
   {
     question:
       "Какие инструменты и методы вы используете для отладки кода JavaScript?",
+    status: true,
   },
   {
     question: "Объясните разницу между изменяемыми и неизменяемыми объектами.",
+    status: false,
   },
   {
     question: "Какой пример неизменяемого объекта можно привести в JavaScript?",
+    status: false,
+    status: true,
   },
   {
     question: "Каковы плюсы и минусы неизменности?",
+    status: true,
   },
   {
     question: "Как можно добиться неизменности собственного кода?",
+    status: false,
   },
   {
     question: "Объясните разницу между синхронными и асинхронными функциями.",
+    status: true,
   },
   {
     question: "Что такое цикл событий?",
+    status: false,
   },
   {
     question: "В чем разница между стеком вызовов и очередью задач?",
+    status: true,
   },
   {
     question:
       "В чем разница между переменными, созданными с помощью let, varили const?",
+    status: false,
   },
   {
     question:
       "Можно ли изменить свойство объекта, определённого через const? Как можно изменить это поведение?",
+    status: true,
   },
   {
     question: "В чем различия между конструкторами классов ES6 и функций ES5?",
+    status: true,
   },
   {
     question:
       "Можете ли вы предложить пример использования нового =>синтаксиса стрелочных функций? Чем этот новый синтаксис отличается от других функций?",
+    status: false,
   },
   {
     question:
       "Какие преимущества дает использование стрелочного синтаксиса для метода в конструкторе?",
+    status: true,
   },
   {
     question: "Каково определение функции высшего порядка?",
+    status: true,
   },
   {
     question:
       "Можете ли вы привести пример деструктуризации объекта или массива?",
+    status: false,
   },
   {
     question:
       "Можете ли вы привести пример генерации строки с использованием шаблонных литералов ES6?",
+    status: true,
   },
   {
     question:
       "Можете ли вы привести пример функции карри и объяснить, почему этот синтаксис даёт преимущество?",
+    status: true,
   },
   {
     question:
       "Каковы преимущества использования spread syntaxи чем оно отличается от rest syntax?",
+    status: false,
   },
   {
     question: "Как можно обмениваться кодом между файлами?",
+    status: true,
   },
   {
     question:
       "Зачем вам может понадобиться создавать статические члены класса?",
+    status: false,
   },
   {
     question:
       "В чем разница между циклами whileи do-whileциклами в JavaScript?",
+    status: true,
   },
   {
     question: "Что такое обещание? Где и как его использовать?",
+    status: true,
   },
   {
     question:
       "Обсудите, как можно использовать принципы объектно-ориентированного программирования при кодировании на JavaScript.",
+    status: false,
   },
   {
     question: "В чем разница между event.targetи event.currentTarget?",
+    status: true,
   },
   {
     question:
       "В чем разница между event.preventDefault()и event.stopPropagation()?",
+    status: true,
   },
 ];
-console.log(questionMassJs);
+// console.log(questionMassJs);
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function renderMass(questions) {
+const renderCard = function renderMass(questions) {
   questions.forEach(function (q, index) {
     const indexNum = index + 1;
     const iconTrue = q.status == true ? "fas fa-check-square" : "fas fa-ban";
@@ -6,17 +6,21 @@ function renderMass(questions) {
     const iconColor = q.status == true ? "has-text-success" : "has-text-danger";
 
     const newCardHTML = `
+    
       <div class="card mt-4">
         <div class="card-content">
           <div class="content">
-            <p class="is-size-5 has-text-weight-semibold">
-              ${indexNum}. ${q.question}
-                  <span class="icon has-text-success">
-          <i class="fas ${iconTrue} ${iconColor}"></i>
-          <p>${textTrue}</p>
-          </span>
-          
-            </p>
+            <div class="is-flex is-justify-content-space-between is-align-items-center">
+              <p class="is-size-5 has-text-weight-semibold mb-0">
+                ${indexNum}. ${q.question}
+              </p>
+              <span class="tag is-medium ${iconColor}">
+                <span class="icon is-small mr-1">
+                  <i class="fas ${iconTrue}"></i>
+                </span>
+                ${textTrue}
+              </span>
+            </div>
           </div>
         </div>
       </div>
@@ -24,7 +28,35 @@ function renderMass(questions) {
 
     contentJs.insertAdjacentHTML("beforeend", newCardHTML);
   });
+};
+
+renderCard(questionMassJs);
+
+const falseStatus = questionMassJs.filter((q) => q.status == false);
+const OffCheck = document.getElementById("OffCheck");
+function offBtn() {
+  contentJs.innerHTML = "";
+  const checkOffBox = OffCheck.checked ? falseStatus : questionMassJs;
+  renderCard(checkOffBox);
 }
 
-renderMass(questionMassJs);
+const trueStatus = questionMassJs.filter((q) => q.status == true);
 
+const onCheck = document.getElementById("OnCheck");
+function onBtn() {
+  contentJs.innerHTML = "";
+  const checkOnBox = onCheck.checked ? trueStatus : questionMassJs;
+  renderCard(checkOnBox);
+}
+
+const writeText = document.getElementById("writeText");
+
+function addNew() {
+  const newText = writeText.value;
+
+  const newQuestion = {
+    question: newText,
+  };
+  questionMassJs.unshift(newQuestion);
+  renderCard(questionMassJs);
+}
